6aec48db86d66dd94b11d6d7f8378f9cdeb1dd34
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-11-28 09:27:59 -0300

    Corrigindo migrations

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -14,7 +14,7 @@ class Series extends Model
 
     protected $fillable = ['nome'];
     
-    public function temporadas()
+    public function season()
     {
         return $this->hasMany(Season::class, 'series_id');
     }

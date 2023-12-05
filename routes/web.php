a976095420fa6d612a7f0e8522a74b457df1aed9
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-12-05 09:03:33 -0300

    Implementando autorizações e iniciando envio de email

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,21 @@ use App\Http\Middleware\Autenticador;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/series');
-})->middleware(Autenticador::class);
 
-Route::resource('/series', SeriesController::class)
+
+Route::middleware('autenticador')->group(function () {
+    Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
-
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+    Route::get('/', function () {
+        return redirect('/series');
+    });
+        
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+    
+    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+});
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('signin');
@@ -36,3 +40,12 @@ Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/register', [UsersController::class, 'create'])->name('users.create');
 Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+
+Route::get('/email', function () {
+    return new \App\Mail\SeriesCreated(
+        'Série de teste',
+        1,
+        5,
+        10,
+    );
+});

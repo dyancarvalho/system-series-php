68051366e2835bacffc46a5fe205ee985a836e04
Dyan Carvalho <soudyancarvalho@gmail.com>
2023-12-12 10:47:59 -0300

    Incluindo imagens nas views

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -4,9 +4,14 @@
     <ul class="list-group">    
             @foreach ($series as $serie) 
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <a href="{{ route ('seasons.index', $serie->id)}}">
-                        {{ $serie->nome }}
-                    </a>
+
+                    <div class="d-flex align-items-center">
+                        <img class="me-3" src="{{ asset('storage/' . $serie->cover) }}" width="100" class="img-thumbnail" alt="">
+                        @auth <a href="{{ route('seasons.index', $serie->id) }}"> @endauth
+                              {{ $serie->nome }}
+                        @auth </a> @endauth
+                    </div>
+
                     <span class="d-flex">
                         <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
                             E
